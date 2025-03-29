71c659c3cee8fe2806fd8fe48d4a2ecf64b08e07
Ayfri <pierre.ayfri@gmail.com>
2025-03-30 00:03:43 +0100

    feat: add home and shop pages

---
+++ src/views/Home.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const newsItems = ref([
+  {
+    date: '03/15/2000',
+    text: 'New Charizard cards in stock! 🔥',
+    gif: '/gifs/pokemon/hot.gif'
+  },
+  {
+    date: '03/10/2000',
+    text: 'Website now optimized for Internet Explorer 5.0!',
+    gif: '/gifs/pokemon/new2.gif'
+  },
+  {
+    date: '03/05/2000',
+    text: 'Added new Guestbook feature - Sign it!',
+    gif: '/gifs/pokemon/new2.gif'
+  }
+])
+</script>
+
+<template>
+  <div>
+    <div class="flex gap-4 items-center mb-8">
+      <img src="/gifs/pokemon/star.gif" alt="Star" class="h-8" />
+      <h2 class="text-2xl font-bold rainbow-text">Welcome to Pokéstore 2000!</h2>
+      <img src="/gifs/pokemon/star.gif" alt="Star" class="h-8" />
+    </div>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div class="retro-card news-container">
+        <div class="flex items-center gap-2 mb-4">
+          <img src="/gifs/pokemon/letter.gif" alt="News" class="h-6" />
+          <h3 class="text-xl font-bold">Latest News</h3>
+        </div>
+        <div class="space-y-4">
+          <div v-for="item in newsItems" :key="item.date" class="border-b-2 border-purple-200 pb-2">
+            <div class="flex items-center gap-2">
+              <img :src="item.gif" alt="Icon" class="h-6" />
+              <div class="font-bold text-blue-700">{{ item.date }}</div>
+            </div>
+            <div class="mt-1">{{ item.text }}</div>
+          </div>
+        </div>
+      </div>
+
+      <div class="retro-card bg-gradient-to-r from-red-100 to-yellow-100">
+        <div class="flex items-center gap-2 mb-4">
+          <img src="/gifs/pokemon/hot.gif" alt="Hot" class="h-6" />
+          <h3 class="text-xl font-bold">Featured Card</h3>
+        </div>
+        <div class="text-center">
+          <div class="card-hover inline-block">
+            <img
+              src="https://assets.pokemon.com/assets/cms2/img/cards/web/SWSH4/SWSH4_EN_20.png"
+              alt="Charizard VMAX"
+              class="mx-auto mb-4 border-4 border-yellow-400 rounded"
+            />
+          </div>
+          <div class="blink text-red-600 font-bold text-xl">
+            <img src="/gifs/pokemon/hot.gif" alt="Fire" class="inline h-6" />
+            HOT ITEM!
+            <img src="/gifs/pokemon/hot.gif" alt="Fire" class="inline h-6" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="mt-8 p-4 under-construction">
+      <div class="bg-gradient-to-r from-yellow-100 to-orange-100 p-4 text-center border-2 border-yellow-400">
+        <img src="/gifs/pokemon/animepika.gif" alt="Under Construction" class="inline-block" />
+        <p class="font-bold mt-2 rainbow-text text-xl">More features coming soon!</p>
+        <img src="/gifs/pokemon/animepika.gif" alt="Under Construction" class="inline-block" />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/Shop.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import PokemonCard from '../components/PokemonCard.vue'
+import { useCartStore } from '../stores/cart'
+
+const cart = useCartStore()
+
+const cards = ref([
+  {
+    id: 1,
+    name: 'Pikachu',
+    price: 29.99,
+    image: 'https://assets.pokemon.com/assets/cms2/img/cards/web/SM10/SM10_EN_55.png',
+    rarity: 'Rare',
+    animated: '/gifs/pokemon/pikachu.gif'
+  },
+  {
+    id: 2,
+    name: 'Charizard',
+    price: 199.99,
+    image: 'https://assets.pokemon.com/assets/cms2/img/cards/web/SWSH4/SWSH4_EN_20.png',
+    rarity: 'Ultra Rare',
+    animated: '/gifs/pokemon/charmander.gif'
+  },
+  {
+    id: 3,
+    name: 'Mewtwo',
+    price: 149.99,
+    image: 'https://assets.pokemon.com/assets/cms2/img/cards/web/SM12/SM12_EN_75.png',
+    rarity: 'Secret Rare',
+    animated: '/gifs/pokemon/animepika.gif'
+  },
+  {
+    id: 4,
+    name: 'Blastoise',
+    price: 89.99,
+    image: 'https://assets.pokemon.com/assets/cms2/img/cards/web/SM12/SM12_EN_25.png',
+    rarity: 'Holo Rare',
+    animated: '/gifs/pokemon/squirtle.gif'
+  },
+  {
+    id: 5,
+    name: 'Venusaur',
+    price: 79.99,
+    image: 'https://assets.pokemon.com/assets/cms2/img/cards/web/SM12/SM12_EN_1.png',
+    rarity: 'Holo Rare',
+    animated: '/gifs/pokemon/bulbasaur.gif'
+  },
+  {
+    id: 6,
+    name: 'Magikarp',
+    price: 0.99,
+    image: 'https://assets.pokemon.com/assets/cms2/img/cards/web/SM9/SM9_EN_32.png',
+    rarity: 'Common',
+    animated: '/gifs/pokemon/star.gif'
+  }
+])
+</script>
+
+<template>
+  <div>
+    <div class="flex items-center mb-6">
+      <img src="/gifs/pokemon/pokeball.gif" alt="Pokeball" class="h-8 mr-3" />
+      <h2 class="text-2xl font-bold">Card Shop</h2>
+      <img src="/gifs/pokemon/pokeball.gif" alt="Pokeball" class="h-8 ml-3" />
+    </div>
+
+    <div class="mb-4 p-2 bg-yellow-100 border border-yellow-500 text-center">
+      <img src="/gifs/pokemon/hot.gif" alt="Hot" class="inline h-6 mr-2" />
+      <span class="font-bold">Flash Sale - Limited Time Only!</span>
+      <img src="/gifs/pokemon/hot.gif" alt="Hot" class="inline h-6 ml-2" />
+    </div>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <PokemonCard
+        v-for="card in cards"
+        :key="card.id"
+        :card="card"
+        @add-to-cart="cart.addItem(card.id)"
+      />
+    </div>
+
+    <div class="mt-8 text-center">
+      <hr class="mb-4" />
+      <div>
+        <img src="/gifs/pokemon/new2.gif" alt="New" class="inline h-6 mr-2" />
+        <span class="font-bold">New Shipment Arriving Soon!</span>
+      </div>
+      <div class="mt-2">
+        <span class="text-red-600">Pre-order now for a special discount!</span>
+      </div>
+    </div>
+  </div>
+</template>
